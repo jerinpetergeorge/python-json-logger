9928f2978069ca40de4bf776266b9eaf4d44cd56
Jerin Peter George <jerinpetergeorge@gmail.com>
2020-12-15 08:46:59 +0530

    'RedisLogHandler' class updated to handle dynamic redis keys

--- redis_json_logger/handler.py
+++ redis_json_logger/handler.py
@@ -4,13 +4,19 @@ import redis
 
 class RedisLogHandler(logging.Handler):
 
-    def __init__(self, key, host='localhost'):
+    def __init__(self, key="log-key-redis", host='localhost'):
         logging.Handler.__init__(self)
         self.key = key
         self.redis_server = redis.Redis(host)
 
+    def get_key(self, record: logging.LogRecord):
+        try:
+            return record.name  # using logger name as `redis key`
+        except AttributeError:
+            return self.key
+
     def emit(self, record):
         try:
-            self.redis_server.rpush(self.key, self.format(record))
+            self.redis_server.rpush(self.get_key(record), self.format(record))
         except:  # noqa
             pass
